dd2791a6c6a6d6d32c20857f0a331a1b60e2cf4b
Dave Warry <dave@warry.org.uk>
2013-07-03 17:49:06 +0100

    Studio rationalised the sql files for Issue and Dependency

--- RaidLog.Db/dbo/Tables/Dependency.sql
+++ RaidLog.Db/dbo/Tables/Dependency.sql
@@ -1,4 +1,26 @@
-﻿CREATE TABLE [dbo].[Dependency]
+﻿CREATE TABLE [dbo].[Dependency](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProjectId] [int] NOT NULL,
+	[DependencyNumber] [int] NOT NULL,
+	[Status] [nvarchar](10) NOT NULL,
+	[Workstream] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](2048) NOT NULL,
+	[PlannedDate] [date] NOT NULL,
+	[RequiredByDate] [date] NOT NULL,
+	[Comments] [nvarchar](2048) NOT NULL,
+	[RAG] [nchar](1) NOT NULL,
+	[DependencyLevel] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Dependency] PRIMARY KEY CLUSTERED 
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Dependency]  WITH CHECK ADD  CONSTRAINT [FK_Dependency_Project] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Project] ([Id])
+GO
+
+ALTER TABLE [dbo].[Dependency] CHECK CONSTRAINT [FK_Dependency_Project]
+GO
--- RaidLog.Db/dbo/Tables/Issue.sql
+++ RaidLog.Db/dbo/Tables/Issue.sql
@@ -1,4 +1,16 @@
-﻿CREATE TABLE [dbo].[Issue]
+﻿CREATE TABLE [dbo].[Issue](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProjectId] [int] NOT NULL,
+	[IssueNumber] [int] NOT NULL,
+	[RaisedDate] [date] NOT NULL,
+	[RaisedBy] [nvarchar](50) NOT NULL,
+	[Owner] [nvarchar](50) NULL,
+	[Workstream] [nvarchar](50) NULL,
+	[Description] [nvarchar](2048) NULL,
+	[ImpactCommentary] [nvarchar](2048) NULL,
+ CONSTRAINT [PK_Issue] PRIMARY KEY CLUSTERED 
 (
-	[Id] INT NOT NULL PRIMARY KEY
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY], 
+    CONSTRAINT [FK_Issue_To_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project]([Id])
+) ON [PRIMARY].
