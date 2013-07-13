9881f0a22547709967e9f885b2dbcdcdee97171b
Dave Warry <dave@warry.org.uk>
2013-07-13 07:38:19 +0100

    Added explicit toastr options - mainly so that they would appear bottom-right, not over the top of the first things you see.

--- RaidLog.Spa/App/main.js
+++ RaidLog.Spa/App/main.js
@@ -26,6 +26,16 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
         http:   true
     });
 
+    toastr.options = {
+        "debug": false,
+        "positionClass": "toast-bottom-right",
+        "onclick": null,
+        "fadeIn": 300,
+        "fadeOut": 1000,
+        "timeOut": 5000,
+        "extendedTimeOut": 1000,
+        "tapToDismiss": true
+    };
 
     ko.validation.init({ decorateElement: false, insertMessages: false, messagesOnModified: false });
 
