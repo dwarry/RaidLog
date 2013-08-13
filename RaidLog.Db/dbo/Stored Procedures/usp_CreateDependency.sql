744cd4268f70d60f6ade1391d6716e15458e1b94
Dave Warry <dave@warry.org.uk>
2013-08-13 08:24:38 +0100

    Table changes and C/U proces for Dependencies

---
+++ RaidLog.Db/dbo/Stored Procedures/usp_CreateDependency.sql
@@ -0,0 +1,60 @@
+﻿CREATE PROCEDURE dbo.usp_CreateDependency
+    @projectId int,
+    @status nvarchar(10),
+    @workstream nvarchar(50),
+    @description nvarchar(2048),
+    @plannedDate date,
+    @requiredByDate date,
+    @comments nvarchar(2048),
+    @ragStatus nvarchar(5),
+    @dependencyLevel nvarchar(50)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+    SET XACT_ABORT ON;
+
+    BEGIN TRANSACTION;
+    
+    DECLARE @dependencyNumbers Table(dependencyNo int);
+    DECLARE	@dependencyNumber int;
+
+    UPDATE  dbo.Project
+    SET     NextDependencyNumber = NextDependencyNumber + 1
+    OUTPUT  deleted.NextDependencyNumber into @dependencyNumbers
+    WHERE   Id = @projectId;
+    
+    if @@ROWCOUNT = 0
+        THROW 50001, 'No Project with that id', 1;
+    
+    select @dependencyNumber = dependencyNo
+    from @dependencyNumbers;	
+
+    INSERT INTO [dbo].[Dependency]
+               ([ProjectId]
+               ,[DependencyNumber]
+               ,[Status]
+               ,[Workstream]
+               ,[Description]
+               ,[PlannedDate]
+               ,[RequiredByDate]
+               ,[Comments]
+               ,[RAG]
+               ,[DependencyLevel])
+     OUTPUT
+               INSERTED.*
+     VALUES
+               (@projectId
+               ,@dependencyNumber
+               ,@status
+               ,@workstream
+               ,@description
+               ,@plannedDate
+               ,@requiredByDate
+               ,@comments
+               ,@ragStatus
+               ,@dependencyLevel);
+
+    COMMIT TRANSACTION;
+
+END
--- RaidLog.Db/dbo/Tables/Dependency.sql
+++ RaidLog.Db/dbo/Tables/Dependency.sql
@@ -1,5 +1,6 @@
-CREATE TABLE [dbo].[Dependency] (
+﻿CREATE TABLE [dbo].[Dependency] (
     [Id]               INT             IDENTITY (1, 1) NOT NULL,
+    [Version]          ROWVERSION      NOT NULL,
     [ProjectId]        INT             NOT NULL,
     [DependencyNumber] INT             NOT NULL,
     [Status]           NVARCHAR (10)   NOT NULL,
@@ -7,12 +8,14 @@ CREATE TABLE [dbo].[Dependency] (
     [Description]      NVARCHAR (2048) NOT NULL,
     [PlannedDate]      DATE            NOT NULL,
     [RequiredByDate]   DATE            NOT NULL,
-    [Comments]         NVARCHAR (2048) NOT NULL,
+    [Comments]         NVARCHAR (2048) NULL,
     [RAG]              NCHAR (1)       NOT NULL,
     [DependencyLevel]  NVARCHAR (50)   NOT NULL,
     CONSTRAINT [PK_Dependency] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Dependency_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([Id])
 );
+
+
 
 
 GO
